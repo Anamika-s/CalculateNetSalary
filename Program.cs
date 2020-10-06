30954680e082870313b0942f209377934ef7db42
Anamika <anamika.sawhney22@gmail.com>
2020-10-06 18:02:00 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateNetSalary
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee1 a = new Employee1();
+            a.getEmployedetail();
+            a.show();
+
+
+        }
+    }
+}
